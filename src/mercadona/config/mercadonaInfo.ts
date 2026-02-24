16cba59a32d056ca37832abf1cd5fd6006ca2ba7
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-24 22:57:05 +0100

    created centralized config constant class

---
+++ src/mercadona/config/mercadonaInfo.ts
@@ -0,0 +1,15 @@
+export const MERCADONA_INFO = {
+  name: "Mercadona",
+  baseUrl: "https://tienda.mercadona.es",
+  warehouse: {
+    endPoint: "/api/postal-codes/actions/change-pc/",
+    header: "x-customer-wh",
+  },
+  algoliaTemplateUrl:
+    "https://{APP_ID}-dsn.algolia.net/1/indexes/products_prod_{WAREHOUSE}_es/query?x-algolia-agent={AGENT}&x-algolia-api-key={API_KEY}&x-algolia-application-id={APP_ID}",
+  algolia: {
+    appId: process.env.MERCADONA_ALGIOLA_APP_ID,
+    apiKey: process.env.MERCADONA_ALGIOLA_API_KEY,
+    agent: process.env.MERCADONA_ALGIOLA_AGENT,
+  },
+} as const;
