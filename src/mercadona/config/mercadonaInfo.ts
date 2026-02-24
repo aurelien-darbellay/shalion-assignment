48c1e962c9ce5359e461b8f94ace9dac44ca0905
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-24 23:17:13 +0100

    added config info for algiola client

--- src/mercadona/config/mercadonaInfo.ts
+++ src/mercadona/config/mercadonaInfo.ts
@@ -7,7 +7,7 @@ export const MERCADONA_INFO = {
   },
   algoliaTemplateUrl:
     "https://{APP_ID}-dsn.algolia.net/1/indexes/products_prod_{WAREHOUSE}_es/query?x-algolia-agent={AGENT}&x-algolia-api-key={API_KEY}&x-algolia-application-id={APP_ID}",
-  algolia: {
+  algoliaKeys: {
     appId: process.env.MERCADONA_ALGIOLA_APP_ID,
     apiKey: process.env.MERCADONA_ALGIOLA_API_KEY,
     agent: process.env.MERCADONA_ALGIOLA_AGENT,
