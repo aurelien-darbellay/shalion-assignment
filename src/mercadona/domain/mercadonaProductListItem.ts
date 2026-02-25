acb61157d845fa96855dd550bbbbfc36bad0e459
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 13:50:52 +0100

    saved mercadonaProductListItem

--- src/mercadona/domain/mercadonaProductListItem.ts
+++ src/mercadona/domain/mercadonaProductListItem.ts
@@ -2,8 +2,6 @@ import { MercadonaProduct } from "./mercadonaProduct";
 
 export type MercadonaProductListItem = {
   product: MercadonaProduct;
-
-  // Search/list context
   position: number; // index in hits[]
   score?: number; // optional, from hit.score (if you want it)
 };
