886644f034a8f064ca018f5fb87243656c835c17
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 16:29:28 +0100

    created test function for the flow retrieving single product page + wired in entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 import "dotenv/config";
 import { createMercadonaNavigator } from "./mercadona/navigator/factory/navigatorFactory";
-import { QueryOptionsBuilder } from "./mercadona/domain/queryOptions";
+import { testProductsListPage } from "./testProductsListPage";
+import { testProductPage } from "./testProductPage";
 
 async function main() {
   const navigator = await createMercadonaNavigator("08024");
-  console.log("Warehouse:", navigator.getWarehouse());
-  const options = QueryOptionsBuilder.builder()
-    .withSearchQuery("champú")
-    .build();
-  const productListPage = await navigator.getProducts(options);
-  console.log("Page retrieved:", productListPage);
-  console.log(
-    "Product in position 0:",
-    productListPage.getProductByPosition(0),
-  );
-  console.log(
-    "Hacendado products on page:",
-    productListPage.getProductsByBrand("Hacendado"),
-  );
+  //await testProductsListPage(navigator);
+  await testProductPage(navigator);
 }
 
 main().catch((err) => {
---
+++ src/testProductPage.ts
@@ -0,0 +1,35 @@
+import { MercadonaNavigator } from "./mercadona/navigator/mercadonaNavigator";
+
+export async function testProductPage(navigator: MercadonaNavigator) {
+  const productId = 26029;
+  const productPage = await navigator.getProductPage(productId);
+  console.log("Product details:");
+  console.log(`Name: ${productPage.product.name}`);
+  console.log(`Brand: ${productPage.product.brand}`);
+  console.log(`Price: ${productPage.product.price.unitPrice} EUROS`);
+  const associatedProducts = productPage.associatedProducts;
+  console.log(`\n ${associatedProducts.length} associated products.`);
+  console.log("\nVisible associated products:");
+  const visibleAssociatedProducts = productPage.visibleItems;
+  visibleAssociatedProducts.forEach((associatedProduct: any, index: number) => {
+    console.log(`\nAssociated Product #${index + 1}:`);
+    console.log(`  Name: ${associatedProduct.product.name}`);
+    console.log(`  Brand: ${associatedProduct.product.brand}`);
+    console.log(`  Price: ${associatedProduct.product.price.unitPrice} EUROS`);
+    console.log(`  URL: ${associatedProduct.product.productUrl}`);
+  });
+  console.log("\nMoving to next page of associated products...");
+  productPage.next();
+  const nextVisibleAssociatedProducts = productPage.visibleItems;
+  nextVisibleAssociatedProducts.forEach(
+    (associatedProduct: any, index: number) => {
+      console.log(`\nAssociated Product #${index + 1} (Page 2):`);
+      console.log(`  Name: ${associatedProduct.product.name}`);
+      console.log(`  Brand: ${associatedProduct.product.brand}`);
+      console.log(
+        `  Price: ${associatedProduct.product.price.unitPrice} EUROS`,
+      );
+      console.log(`  URL: ${associatedProduct.product.productUrl}`);
+    },
+  );
+}
