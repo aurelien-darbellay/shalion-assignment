34e3dbbeb5c99afed1aa959971e7f731aa70f286
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 14:42:11 +0100

    modified index for testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { createMercadonaNavigator } from "./mercadona/navigator/factory/navigatorFactory";
+import { QueryOptionsBuilder } from "./mercadona/domain/queryOptions";
 
 async function main() {
-  const navigator = await createMercadonaNavigator("46001");
+  const navigator = await createMercadonaNavigator("08024");
   console.log("Warehouse:", navigator.getWarehouse());
-  const products = await navigator.getProducts("leche");
-  console.log("Products:", products.data);
+  const options = QueryOptionsBuilder.builder()
+    .withSearchQuery("champú")
+    .build();
+  const productListPage = await navigator.getProducts(options);
+  console.log("Page retrieved:", productListPage);
 }
 
 main().catch((err) => {
