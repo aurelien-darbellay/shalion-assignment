4f8eb77ca2a871efb71bd03312e889bed256cb67
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 09:53:20 +0100

    let error creating navigator bubble up

--- src/mercadona/navigator/factory/navigatorFactory.ts
+++ src/mercadona/navigator/factory/navigatorFactory.ts
@@ -10,19 +10,14 @@ export async function createMercadonaNavigator(
   postalCode: string,
 ): Promise<MercadonaNavigator> {
   let mercadonaNavigator: MercadonaNavigator;
-  try {
-    const mercadonaClient = createMercadonaClient();
-    const warehouse = await resolveWarehouse(mercadonaClient, postalCode);
-    const algoliaClient = createMercadonaAlgoliaClient(warehouse);
-    mercadonaNavigator = new MercadonaNavigator(
-      mercadonaClient,
-      algoliaClient,
-      warehouse,
-    );
-  } catch (error: any) {
-    console.error("Error creating MercadonaNavigator:", error.message);
-    throw error;
-  }
+  const mercadonaClient = createMercadonaClient();
+  const warehouse = await resolveWarehouse(mercadonaClient, postalCode);
+  const algoliaClient = createMercadonaAlgoliaClient(warehouse);
+  mercadonaNavigator = new MercadonaNavigator(
+    mercadonaClient,
+    algoliaClient,
+    warehouse,
+  );
 
   return mercadonaNavigator;
 }
