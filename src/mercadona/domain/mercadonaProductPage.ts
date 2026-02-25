14d0cb3b68595c6798f0279425d3afb50ea9fcdb
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 16:26:51 +0100

    created mercadonaProductPage

---
+++ src/mercadona/domain/mercadonaProductPage.ts
@@ -0,0 +1,39 @@
+import { MercadonaProduct } from "./mercadonaProduct";
+import { MercadonaProductListItem } from "./mercadonaProductListItem";
+
+export class MercadonaProductPage {
+  private start = 0; // zero-based internally
+  private readonly pageSize = 5;
+
+  constructor(
+    public product: MercadonaProduct,
+    public associatedProducts: MercadonaProductListItem[],
+  ) {}
+
+  static fromProductAndAssociated(
+    product: MercadonaProduct,
+    associatedProducts: MercadonaProductListItem[],
+  ): MercadonaProductPage {
+    return new MercadonaProductPage(product, associatedProducts);
+  }
+
+  get visibleItems(): MercadonaProductListItem[] {
+    return this.associatedProducts.slice(
+      this.start,
+      this.start + this.pageSize,
+    );
+  }
+
+  next(): void {
+    if (this.start + this.pageSize > this.associatedProducts.length) {
+      return;
+    }
+    this.start += this.pageSize;
+  }
+  previous(): void {
+    if (this.start === 0) {
+      return;
+    }
+    this.start -= this.pageSize;
+  }
+}
