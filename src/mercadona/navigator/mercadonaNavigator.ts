e4c86e5f6f9bab449fd3beebe95487c8b57aca79
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-02-25 14:12:58 +0100

    wired mapper in navigator

--- src/mercadona/navigator/mercadonaNavigator.ts
+++ src/mercadona/navigator/mercadonaNavigator.ts
@@ -6,20 +6,28 @@ export class MercadonaNavigator {
   private readonly mercadonaClient: AxiosInstance;
   private readonly algoliaClient: AxiosInstance;
   private readonly warehouse: string = "vlc01";
+  private readonly zipCode: string = "46001";
 
   constructor(
     mercadonaClient: AxiosInstance,
     algoliaClient: AxiosInstance,
     warehouse: string,
+    zipCode: string,
   ) {
     this.mercadonaClient = mercadonaClient;
     this.algoliaClient = algoliaClient;
     this.warehouse = warehouse;
+    this.zipCode = zipCode;
   }
 
   getWarehouse(): string {
     return this.warehouse;
   }
+
+  getZipCode(): string {
+    return this.zipCode;
+  }
+
   getProducts(options: QueryOptions) {
     const request = createAlgoliaSearchRequest(options);
     return this.algoliaClient.post("", request);
